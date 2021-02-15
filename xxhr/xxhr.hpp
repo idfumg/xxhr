05ee7d6049325e5267565e82242833f92bb4fe30
Lambourl <65184664+Lambourl@users.noreply.github.com>
2021-02-15 16:17:57 +0100

    :wrench: delete boost use windows

--- xxhr/xxhr.hpp
+++ xxhr/xxhr.hpp
@@ -3,14 +3,11 @@
 
 #define XXHR_VERSION "v0.0.1"
 #if defined(_WIN32)
-#ifndef BOOST_USE_WINDOWS_H 
-#define BOOST_USE_WINDOWS_H 
-#endif
 # undef X509_NAME
 # undef X509_CERT_PAIR
 # undef X509_EXTENSIONS
-//#undef OCSP_REQUEST
-//#undef OCSP_RESPONSE
+#undef OCSP_REQUEST
+#undef OCSP_RESPONSE
 #endif
 
 #include "api.hpp"
